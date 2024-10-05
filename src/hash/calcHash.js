ba1b9c902706d0b8f1e55c7bbd6b156a9ee6de05
anastasiiavorobiova <anastasiiavorobiova2403@gmail.com>
2024-10-06 00:20:30 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'node:crypto';
+import path from 'node:path';
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const input = createReadStream(file);
+
+  input.on('readable', () => {
+    const data = input.read();
+
+    if (data) {
+      hash.update(data);
+      console.log(data.toString('hex'));
+    }
+  });
 };
 
 await calculateHash();
