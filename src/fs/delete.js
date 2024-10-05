70bc783e890a400f9e168e26c636182c0601f6c5
anastasiiavorobiova <anastasiiavorobiova2403@gmail.com>
2024-10-06 00:18:20 +0300

    feat: fs

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,32 @@
+import { rm, access } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const UNEXIST_ERROR_MESSAGE = 'FS operation failed';
+const UNKNOWN_ERROR_MESSAGE = 'Something went wrong during deleting';
+const SUCCESS_MESSAGE = 'The file has been deleted';
+
 const remove = async () => {
-    // Write your code here 
+  const pathToFile = path.resolve(__dirname, 'files', 'fileToRemove.txt');
+
+  const isExist = await access(pathToFile)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!isExist) {
+    throw new Error(UNEXIST_ERROR_MESSAGE);
+  }
+
+  await rm(pathToFile)
+    .then(() => {
+      console.log(SUCCESS_MESSAGE);
+    })
+    .catch(() => {
+      console.log(UNKNOWN_ERROR_MESSAGE);
+    });
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,37 @@
+import { rename as renaming, access } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const EXIST_ERROR_MESSAGE = 'FS operation failed';
+const UNKNOWN_ERROR_MESSAGE = 'Something went wrong during renaming';
+const SUCCESS_MESSAGE = 'The file has been renamed';
+
 const rename = async () => {
-    // Write your code here 
+  const fromFile = path.resolve(__dirname, 'files', 'wrongFilename.txt');
+  const toFile = path.resolve(__dirname, 'files', 'properFilename.md');
+
+  const isExistFromFile = await access(fromFile)
+    .then(() => true)
+    .catch(() => false);
+
+  const isExistToFile = await access(toFile)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!isExistFromFile || isExistToFile) {
+    throw new Error(EXIST_ERROR_MESSAGE);
+  }
+
+  await renaming(fromFile, toFile)
+    .then(() => {
+      console.log(SUCCESS_MESSAGE);
+    })
+    .catch((err) => {
+      console.log(err, UNKNOWN_ERROR_MESSAGE);
+    });
 };
 
 await rename();
