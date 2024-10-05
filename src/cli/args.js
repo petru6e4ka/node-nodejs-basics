309f0519898b2e78de81c0d9b37389de36186459
anastasiiavorobiova <anastasiiavorobiova2403@gmail.com>
2024-10-06 00:18:57 +0300

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const [, , ...args] = process.argv;
+
+  console.log(
+    args
+      .reduce((prev, elem, i) => {
+        if (i % 2) {
+          return (prev += `${elem}, `);
+        }
+
+        return (prev += `${elem.slice(2)} is `);
+      }, '')
+      .slice(0, -2)
+  );
 };
 
 parseArgs();
