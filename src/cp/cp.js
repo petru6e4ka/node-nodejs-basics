d52396108eefe94956334cdec2e2eedfc778a790
anastasiiavorobiova <anastasiiavorobiova2403@gmail.com>
2024-10-07 23:24:23 +0300

    feat: child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import process from 'node:process';
+import { spawn } from 'node:child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const childProcFile = path.resolve(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn('node', [childProcFile, ...args]);
+
+  child.stdout.setEncoding('utf8');
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
